d8e807faab246e1223e1357493a8b075a5f9c852
Gilberto-GBR12 <gilberto.ggbr@gmail.com>
2023-03-06 12:42:21 -0700

    creacion de lo basico
    modelo, vista, controlador

---
+++ controlador/controlador.php
@@ -0,0 +1,27 @@
+<?php
+    class Controlador
+    {
+        //metodo que carge la platilla
+        static public function pagina()
+        {
+            include("vistas/plantilla.php");
+        }
+
+        //metodo que gestiona los enlaces
+        static public function enlacespagina()
+        {
+            if(isset($_GET['accion']))
+            {
+                $enlace = $_GET['accion'];
+            }
+            else
+            {
+                $enlace = "principal";
+            }
+            $respuesta = Paginas::enlacesPaginas($enlaces);
+
+            //muestra la pagina
+            include $respuesta;
+        }
+    }
+?>
